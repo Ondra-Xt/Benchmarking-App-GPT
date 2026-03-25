3f87746a6b53d889efefa7e82b620c7b4e41c618
Ondra-Xt <ocundr@gmail.com>
2026-03-25 12:22:48 +0100

    Treat Geberit system pages as listing intermediates

--- src/connectors/__init__.py
+++ src/connectors/__init__.py
@@ -1,7 +1,11 @@
 # src/connectors/__init__.py
-from . import hansgrohe, dallmer
+from . import hansgrohe, dallmer, tece, aco, viega, geberit
 
 CONNECTORS = {
     "hansgrohe": hansgrohe,
     "dallmer": dallmer,
+    "tece": tece,
+    "aco": aco,
+    "viega": viega,
+    "geberit": geberit,
 }
